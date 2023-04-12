11ab96ef5212116087d3c50d12b100d0226abe0e
Thaysa <thaysarafaele@gmail.com>
2023-04-12 16:37:25 -0400

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <h1 className="title">Hello World!</h1>
+      <GlobalStyle />
     </div>
   );
 }
