1b3a6271f2a22e168a9cb8d8dfc8ed2eb35cc626
Thaysa <thaysarafaele@gmail.com>
2023-04-12 17:59:08 -0400

    criação e estilização da TransactionsTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsTable } from "./components/TransactionsTable";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
@@ -7,6 +8,7 @@ export function App() {
     <>
       <Header />
       <Dashboard />
+      <TransactionsTable />
       <GlobalStyle />
     </>
   );
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento Website</td>
+                        <td className="income">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>12/04/2023</td>
+                    </tr>
+                    <tr>
+                        <td>ALuguel</td>
+                        <td className="outcome">- R$1.100</td>
+                        <td>Casa</td>
+                        <td>10/04/2023</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
