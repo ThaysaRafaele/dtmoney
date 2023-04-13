5122114fa662b9f778f029e2181db16a61bbe60e
Thaysa <thaysarafaele@gmail.com>
2023-04-13 11:51:28 -0400

    estrutura do form e estilização inicial do modal

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    max-width: 1120px;
-    margin:  0 auto;
-    padding: 2.5rem 1 rem;
+export const Container = styled.form`
+    
 `;
